556e2e093c5c45f4f9f1e5405ffb711a974633db
sabarishn <sabarishn@sabarishns-MBP.attlocal.net>
2025-07-01 15:18:32 -0400

    Init eventhubs eventgrid & service bus

---
+++ azure_event_hubs/code/Program.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Identity;
+using System.Text;
+
+
+string namespaceURL = "namespace1";
+string eventHubName = "eventhub1";
+
+DefaultAzureCredentialOptions options = new()
+{
+    ExcludeEnvironmentCredential = true,
+    ExcludeManagedIdentityCredential = true
+};
+
+int numOfEvents = 3;
+
+EventHubProducerClient producerClient = new EventHubProducerClient(
+    namespaceURL,
+    eventhub1,
+    new DefaultAzureCredentialOptions(options)
+);
+
---
+++ service_bus/code/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+queueClient.CreateIfNotExists();
+
+if(queueClient.Exists())
+{
+    queueClient.SendMessage("Message1");
+}
